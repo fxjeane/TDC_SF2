1de3d9c042d56d22416d8aae5409f73e3e44d2de
Rudy Cortes <fxjeane@gmail.com>
2014-04-15 10:14:46 -0700

    Update config.inc.php

--- src/tdc/PlayerBundle/Libs/aws/sdk-1.5.0.1/config.inc.php
+++ src/tdc/PlayerBundle/Libs/aws/sdk-1.5.0.1/config.inc.php
@@ -54,11 +54,11 @@ CFCredentials::set(array(
 
 		// Amazon Web Services Key. Found in the AWS Security Credentials. You can also pass
 		// this value as the first parameter to a service constructor.
-		'key' => 'AKIAIIIZTKDUGDTAQUUA',
+		'key' => 'XXX',
 
 		// Amazon Web Services Secret Key. Found in the AWS Security Credentials. You can also
 		// pass this value as the second parameter to a service constructor.
-		'secret' => 'hpEvRMNC0NuBKRzYeMzhJymoC0aLwR/FeEab71wo',
+		'secret' => 'XXX',
 
 		// This option allows you to configure a preferred storage type to use for caching by
 		// default. This can be changed later using the set_cache_config() method.
